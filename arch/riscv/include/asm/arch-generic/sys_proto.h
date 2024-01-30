74d876eee8a08590dd5a97640d14d58896673171
Mayuresh Chitale <mchitale@ventanamicro.com>
2024-01-30 10:21:16 +0000

    arch: Risc-V: Add empty sys_proto.h
    When CONFIG_MMC_SDHCI_ZYNQ is enabled for Risc-V based boards the build
    fails with the error below:
    
    drivers/mmc/zynq_sdhci.c:17:10: fatal
    error: asm/arch/sys_proto.h: No such file or directory
       17 | #include <asm/arch/sys_proto.h>
          |          ^~~~~~~~~~~~~~~~~~~~~~
    
    Added an empty sys_proto.h to fix the build.
    
    Signed-off-by: Mayuresh Chitale <mchitale@ventanamicro.com>

---
+++ arch/riscv/include/asm/arch-generic/sys_proto.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2024 Ventana Micro Systems
+ */
+
+#ifndef _ASM_ARCH_SYS_PROTO_H
+#define _ASM_ARCH_SYS_PROTO_H
+
+#endif /* _ASM_ARCH_SYS_PROTO_H */
