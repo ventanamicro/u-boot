f6165c1a513ac72c20fb9f2f3de3e14064809cb7
Mayuresh Chitale <mchitale@ventanamicro.com>
2025-02-14 07:57:35 +0000

    finally building
    Signed-off-by: Mayuresh Chitale <mchitale@ventanamicro.com>

--- include/image.h
+++ include/image.h
@@ -139,6 +139,7 @@ enum {
 	IH_ARCH_X86_64,			/* AMD x86_64, Intel and Via */
 	IH_ARCH_XTENSA,			/* Xtensa	*/
 	IH_ARCH_RISCV,			/* RISC-V */
+	IH_ARCH_RISCV64,		/* RISC-V 64 bit*/
 
 	IH_ARCH_COUNT,
 };
